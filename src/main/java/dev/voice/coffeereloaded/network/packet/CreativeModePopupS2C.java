8db5c438f7120f34d9f0a08480022963fd1ebfe8
Voice <noobyaye@gmail.com>
2023-04-04 18:27:24 -0400

    added network handle

--- src/main/java/dev/voice/coffeereloaded/network/packet/CreativeModePopupS2C.java
+++ src/main/java/dev/voice/coffeereloaded/network/packet/CreativeModePopupS2C.java
@@ -1,6 +1,10 @@
 package dev.voice.coffeereloaded.network.packet;
 
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraftforge.network.NetworkEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Supplier;
 
 public class CreativeModePopupS2C {
     public CreativeModePopupS2C() {
@@ -13,5 +17,13 @@ public class CreativeModePopupS2C {
 
     public void toBytes(FriendlyByteBuf buffer) {
 
+    }
+    
+    public boolean handle(@NotNull Supplier<NetworkEvent.Context> supplier) {
+        NetworkEvent.Context context = supplier.get();
+        
+        assert context!= null;
+        
+        return true;
     }
 }
