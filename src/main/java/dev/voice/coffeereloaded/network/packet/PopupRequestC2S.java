568f9b354a6b75ad28a196ac47b819007483563f
Voice <noobyaye@gmail.com>
2023-04-04 18:30:41 -0400

    added logger if player is null

--- src/main/java/dev/voice/coffeereloaded/network/packet/PopupRequestC2S.java
+++ src/main/java/dev/voice/coffeereloaded/network/packet/PopupRequestC2S.java
@@ -1,36 +1,41 @@
 package dev.voice.coffeereloaded.network.packet;
 
+import dev.voice.coffeereloaded.CoffeeReloaded;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.network.NetworkEvent;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Supplier;
 
-public class ClientToServerPacket {
-    public ClientToServerPacket() {
-
+public class PopupRequestC2S {
+    public PopupRequestC2S() {
+    
     }
-
-    public ClientToServerPacket(FriendlyByteBuf buf) {
-
+    
+    public PopupRequestC2S(FriendlyByteBuf buf) {
+    
     }
-
+    
     public void toBytes(FriendlyByteBuf buf) {
-
+    
     }
-
+    
     public boolean handle(@NotNull Supplier<NetworkEvent.Context> supplier) {
         NetworkEvent.Context context = supplier.get();
-
+        
+        assert context!= null;
+        
         context.enqueueWork(() -> {
-
+            
             ServerPlayer player = context.getSender();
-
-            assert player != null;
+            
+            if (player == null) {
+                CoffeeReloaded.LOGGER.error("Player is null");
+            }
         });
-
+        
         return true;
     }
 }
